46c9a2f26104865836d1c5838ddfb4d5c17e1ce5
khampton6 <khampton6@gmail.com>
2012-12-01 22:49:50 -0500

    Added Preview Player

---
+++ FlipSketch/StartScreen/PreviewPlayer.h
@@ -0,0 +1,13 @@
+//
+//  PreviewPlayer.h
+//  FlipSketch
+//
+//  Created by Kevin Hampton on 12/1/12.
+//  Copyright (c) 2012 Kevin Hampton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PreviewPlayer : UIView
+
+@end
